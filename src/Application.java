79fd233633a4717f96cfc58c076df665fd757084
Ivan <ivanporras@inspladelestany.cat>
2021-03-02 16:23:10 +0100

    Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Application {
+	
+	public static void main(String [] args){
+		Scanner sc = new Scanner (System.in);
+		
+		String[][] stock = new String[3][4];//creem una matriu per veure el stock
+		
+		int n = 0;
+		
+		while(n<5)//while per cridar a les diferents funcions dels altres fitxers
+		{
+			estructuratauler(stock);
+			contingutmaquinan(stock);
+		}
+		
+	}
 
 }
