f554ebce114178ea86cb607d5cdfd09e49276f18
Ivan <ivanporras@inspladelestany.cat>
2021-03-04 17:16:28 +0100

    Manteniment.java

--- src/Manteniment.java
+++ src/Manteniment.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
 
 public class Manteniment {
+	
+	public static final int contrasenya = 123456;
+	
+	public static void MantenimentMaquina(String[][] taulastock, int[] productesrestants)
+	{
+		while(stock.contingutmaquina(taulastock) == 0)
+		{
+			int NovaQuantitat = 0;
+			
+			System.out.println("Ingresi la contrasenya per posar stock del producte amb quantitat 0: ");
+			contra = sc.nextInt();
+			
+			if(contrasenya == contra)
+			{
+				
+				System.out.println("Introdueixi la nova quantitat de stock: ");
+				NovaQuantitat = sc.nextInt();
+			}
+		}
+	}
+	
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 
 }
